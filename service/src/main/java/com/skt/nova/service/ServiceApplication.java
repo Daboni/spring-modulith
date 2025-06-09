8ce1749f90c1af75d1072dfc8408ea39d755e16e
jdbc4497 <jdbc4497@sk.com>
2025-06-09 14:44:42 +0900

    init

---
+++ service/src/main/java/com/skt/nova/service/ServiceApplication.java
@@ -0,0 +1,15 @@
+package com.skt.nova.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.skt.nova")
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
---
+++ service/src/test/java/com/skt/nova/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skt.nova.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
